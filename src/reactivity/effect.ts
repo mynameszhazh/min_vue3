f46b0bc82ac5ea99c1aa090d79eed114e1cccf5a
mynameszhazh <1594776259@qq.com>
2023-01-12 00:09:55 +0800

    feat: 基本单测加vue3结合

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,33 @@
+import { extend } from './../shared/index';
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, public scheduler?: Function) {
     this._fn = fn;
   }
   run() {
     activeEffect = this;
-    this._fn();
-    return this._fn;
+    // this._fn();
+    return this._fn();
   }
+
+  stop() {
+    if (this.active) {
+      if (this.onStop) {
+        this.onStop();
+      }
+      clearnEffect(this);
+      this.active = false;
+    }
+  }
+}
+
+function clearnEffect(effect) {
+  effect.deps.forEach((dep) => {
+    dep.delete(effect);
+  });
 }
 
 const targetMap = new Map();
@@ -17,27 +37,47 @@ export function track(target, key) {
     depsMap = new Map();
     targetMap.set(target, depsMap);
   }
-  let deps = depsMap.get(key);
-  if (!deps) {
-    deps = new Set();
-    depsMap.set(key, deps);
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
   }
-  deps.add(activeEffect);
+
+  if(!activeEffect) return
+
+  // 双向数据保存
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
+
 export function trigger(target, key) {
   if (targetMap.has(target)) {
     let depsMap = targetMap.get(target);
     if (depsMap.has(key)) {
       let deps = depsMap.get(key);
       for (let fn of deps) {
-        fn.run();
+        if (fn.scheduler) {
+          fn.scheduler();
+        } else {
+          fn.run();
+        }
       }
     }
   }
 }
 let activeEffect;
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn);
-  let runner = _effect.run();
-  return runner.bind(_effect);
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+
+  extend(_effect, options)
+
+  _effect.run();
+
+  let runner: any = _effect.run.bind(_effect);
+  runner.effec = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effec.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from "../effect";
+import { effect, stop } from "../effect";
 import { reactive } from "../reactive";
 
 describe("effect", () => {
@@ -23,13 +23,76 @@ describe("effect", () => {
     const ob = reactive({
       foo: 123,
     });
-    let num 
+    let num;
     let runner = effect(() => {
-      num = ob.foo + 1
-      return 'foo'
-    })
+      num = ob.foo + 1;
+      return "foo";
+    });
 
-    expect(num).toBe(124)
-    expect(runner()).toBe('foo')
+    expect(num).toBe(124);
+    expect(runner()).toBe("foo");
+  });
+
+  it("scheduler", () => {
+    let dummy;
+    let run;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+
+    let obj = reactive({
+      foo: 1,
+    });
+
+    let runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      {
+        scheduler,
+      }
+    );
+
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    obj.foo++;
+    obj.foo++;
+    obj.foo++;
+    expect(scheduler).toBeCalledTimes(3);
+    expect(dummy).toBe(1);
+    run();
+    expect(dummy).toBe(4);
+  });
+
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
+    obj.prop = 3;
+    expect(dummy).toBe(2);
+
+    runner();
+    expect(dummy).toBe(3);
+  });
+
+  it("onStop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const onStop = jest.fn();
+    const runner = effect(
+      () => {
+        dummy = obj.prop;
+      },
+      {
+        onStop,
+      }
+    );
+    stop(runner);
+    expect(onStop).toBeCalledTimes(1);
   });
 });
