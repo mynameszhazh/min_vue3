9905d2e3e4e408c72d2d5420384953b3ef9e51d9
mynameszhazh <1594776259@qq.com>
2023-02-01 22:15:39 +0800

    feat: nested reactive readonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
+import { isObject } from "../shared";
 import { track, trigger } from "./effect";
-import { ReactiveFlags } from "./reactive";
+import { reactive, ReactiveFlags, readonly } from "./reactive";
 
 const get = createGetter();
 const set = createSetter();
@@ -7,17 +8,23 @@ const readonlyGet = createGetter(true);
 
 function createGetter(isReadonly = false) {
   return function get(target, key) {
-    const res = Reflect.get(target, key);
-
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly;
     } else if (key === ReactiveFlags.IS_READONLY) {
       return isReadonly;
     }
-
     if (!isReadonly) {
       track(target, key);
     }
+
+    const res = Reflect.get(target, key);
+    // 递归处理响应式
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
+
+    
+
     return res;
   };
 }
