52dc2e6466b5fda7f6fd623a37eb96c4b554c9e4
mynameszhazh <1594776259@qq.com>
2023-02-02 23:17:16 +0800

    feat: 添加 computed 的实现

---
+++ src/reactivity/computed.ts
@@ -0,0 +1,31 @@
+import { ReactiveEffect } from "./effect";
+import { hasChanged } from "./../shared/index";
+export function computed(getter) {
+  return new ComputeRefImpl(getter);
+}
+
+class ComputeRefImpl {
+  private _getter: any;
+  private _dirty: boolean = true;
+  private _value: any;
+  private _effect: ReactiveEffect;
+
+  constructor(getter) {
+    this._getter = getter;
+
+    // 把 computed 的函数 当做一个 effect 一样使用
+    this._effect = new ReactiveEffect(getter, () => {
+      if (!this._dirty) {
+        this._dirty = true;
+      }
+    });
+  }
+
+  get value() {
+    if (this._dirty) {
+      this._dirty = false;
+      this._value = this._effect.run();
+    }
+    return this._value;
+  }
+}
