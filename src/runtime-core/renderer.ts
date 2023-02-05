b1d00a1317e7c5fb0c7b4e4cb0ec5cf3294ae978
mynameszhazh <1594776259@qq.com>
2023-02-05 23:05:48 +0800

    feat: 添加 shapeflags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { ShapeFlags } from "./../shared/shapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -6,7 +7,8 @@ export function render(vnode, container) {
 }
 
 export function patch(vnode, container) {
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
   } else {
     processComponent(vnode, container);
@@ -37,10 +39,10 @@ function setupRenderEffect(instance: any, ininalvnode, container) {
 function processElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
 
-  const { children } = vnode;
-  if (typeof children === "string") {
+  const { children, shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     // vonode
     mountChildren(vnode, el);
   }
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,8 +1,22 @@
+import { ShapeFlags } from "../shared/shapeFlags";
+
 export function createVnode(type, props?, children?) {
   let vnode = {
     type,
     props,
+    shapeFlag: getShapeFlag(type),
     children,
   };
+
+  if (typeof children === "string") {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
+  } else if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
+  }
   return vnode;
+}
+function getShapeFlag(type: any) {
+  return typeof type === "string"
+    ? ShapeFlags.ELEMENT
+    : ShapeFlags.STATEFUL_COMPONENT;
 }
